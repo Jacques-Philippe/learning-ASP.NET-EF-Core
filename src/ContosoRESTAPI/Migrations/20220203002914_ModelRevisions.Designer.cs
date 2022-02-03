979e21ee5463aa8e295700d1e3cf6a5dd733b3e1
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-02-02 19:37:08 -0500

    Revise entities
    - Add Required attribute to Name for Pizza, Topping, and Sauce
    - Add Many-to-Many relation between Pizza and Topping
    - Add isVegan property to Sauce
    - Created new migration to reflect the above, and applied it

--- src/ContosoRESTAPI/Migrations/20220203002914_ModelRevisions.Designer.cs
+++ src/ContosoRESTAPI/Migrations/20220203002914_ModelRevisions.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ContosoRESTAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContosoRESTAPI.Migrations
 {
     [DbContext(typeof(PizzaContext))]
-    partial class PizzaContextModelSnapshot : ModelSnapshot
+    [Migration("20220203002914_ModelRevisions")]
+    partial class ModelRevisions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
