4dd3fa9b0cdebb17c3d480d30c848029dbb3fed7
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-02-02 19:57:19 -0500

    Define PizzaService CRUS ops

--- src/ContosoRESTAPI/Services/PizzaService.cs
+++ src/ContosoRESTAPI/Services/PizzaService.cs
@@ -1,29 +1,56 @@
+using ContosoRESTAPI.Data;
 using ContosoRESTAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoRESTAPI.Services;
 
 public class PizzaService
 {
-    public PizzaService() { }
+    private readonly PizzaContext _context;
+
+    //When the PizzaService instance is created, a PizzaContext will be injected as a dependency
+    public PizzaService(PizzaContext context)
+    {
+        _context = context;
+    }
 
     public IEnumerable<Pizza> GetAll()
     {
-        throw new NotImplementedException();
+        //AsNoTracking instructs EF Core to disable change tracking, to optimize performance.
+        return _context.Pizzas.AsNoTracking().ToList();
     }
 
     public Pizza? GetById(int id)
     {
-        throw new NotImplementedException();
+        return _context.Pizzas
+            .Include(p => p.Toppings) //include Toppings field
+            .Include(p => p.Sauce) //include Sauce field
+            .AsNoTracking()
+            .SingleOrDefault(p => p.Id == id); //for given id return default or the pizza
     }
 
     public Pizza? Create(Pizza newPizza)
     {
-        throw new NotImplementedException();
+        // add the new pizza to the EF Core's object graph
+        _context.Pizzas.Add(newPizza);
+        // instruct EF core to persist the object changes to the db
+        _context.SaveChanges();
+
+        return newPizza;
     }
 
     public void DeleteById(int id)
     {
-        throw new NotImplementedException();
+        var pizzaToDelete = _context.Pizzas.Find(id);
+        if (pizzaToDelete is null)
+        {
+            throw new NullReferenceException("Pizza does not exist 😭");
+        }
+        else
+        {
+            _context.Pizzas.Remove(pizzaToDelete);
+            _context.SaveChanges();
+        }
     }
 
     public void AddTopping(int pizzaId, int toppingId)
@@ -33,6 +60,16 @@ public class PizzaService
 
     public void UpdateSauce(int pizzaId, int sauceId)
     {
-        throw new NotImplementedException();
+        // ensure pizza and sauce both exist
+        var pizzaToUpdate = _context.Pizzas.Find(pizzaId);
+        var sauceToUpdate = _context.Sauces.Find(sauceId);
+        if (pizzaToUpdate is null || sauceToUpdate is null)
+        {
+            throw new NullReferenceException("Pizza or sauce does not exist 😭");
+        }
+        // update pizza's sauce
+        pizzaToUpdate.Sauce = sauceToUpdate;
+        // instruct EF core to persist the object changes to the db
+        _context.SaveChanges();
     }
 }
